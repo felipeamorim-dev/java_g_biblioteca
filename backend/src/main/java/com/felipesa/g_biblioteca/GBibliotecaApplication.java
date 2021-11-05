e35aa3086c0eb15b73b905772926e42cf03c4de6
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-11-04 21:12:29 -0300

    Criando projeto

---
+++ backend/src/main/java/com/felipesa/g_biblioteca/GBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.felipesa.g_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GBibliotecaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/felipesa/g_biblioteca/GBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesa.g_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
