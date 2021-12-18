8fbde5a21f35c349a9da63c9b765c829ecffa1ce
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-12-18 20:02:31 -0300

    Controller para editar os livros que serão emprestados completo

--- backend/src/main/java/com/felipesa/g_biblioteca/controllers/CarController.java
+++ backend/src/main/java/com/felipesa/g_biblioteca/controllers/CarController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,6 +46,18 @@ public class CarController {
 				.toUri();
 		loanService.insertCar(bookloanDto);
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Void> deleteCar(@PathVariable Long id){
+		carService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/delete/all")
+	public ResponseEntity<Void> deleteAllCar(){
+		carService.deleteAll();
+		return ResponseEntity.noContent().build();
 	}
 	
 }
